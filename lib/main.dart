2e27870613c02a76e6118fa0d580e1f8440147e8
ahmedhossam6666 <a.youseff627@gmail.com>
2024-07-10 21:16:44 +0100

    app init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:islamyapp/homescreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routName,
+      routes: {
+        HomeScreen.routName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
